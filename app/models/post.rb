156e6faebd4380f53b8191feb80c264ce945b328
kwizl <karis.njoroge@gmail.com>
2020-04-18 01:46:41 +0300

    Rectify linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  
+
   validates :title, presence: true, length: { minimum: 5, maximum: 60 }
   validates :body, presence: true
 end
